cdfbc8e92fd851c70bf9121d3ec6ec5b647eec91
hashaam <hashaam@gmail.com>
2023-10-24 17:55:50 +0500

    Tried full screen but did not liked it so reverted back

--- alien_invastion.py
+++ alien_invastion.py
@@ -13,9 +13,7 @@ class AlienInvasion:
         # Settings class instance used to dynamically change the self.screen attribute
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_width))
         pygame.display.set_caption("Hashaam's Game")
         #Creating the instance of the Ship class from ship.py
         self.ship = Ship(self)
