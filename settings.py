d9bd7352c534a9130ac177a8ab783ab3a8d2ccc3
hashaam <hashaam@gmail.com>
2023-10-24 17:49:09 +0500

    Refactored KEYUP and KEYDOWN events handlers in helper functions

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 class Settings:
     """A class to store all the settings for Hashaam's game"""
     def __init__(self):
+        # Game window screen size
         self.screen_width = 750
         self.screen_height = 800
+        
+        # Screen background color
         self.bg_color = (230, 230, 230)
+        
+        # Ship speed settings
+        self.ship_speed = 7
