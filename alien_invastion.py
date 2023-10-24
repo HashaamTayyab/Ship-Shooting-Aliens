ef69897ceb3f7c157223b2e6e04bdff2628e88d6
hashaam <hashaam@gmail.com>
2023-10-24 16:55:54 +0500

    Added the right key movement functionality

--- alien_invastion.py
+++ alien_invastion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         while True:
             #Helper function call (It can only be used within the class)
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
             
@@ -35,8 +36,14 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
-                
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                    
+                      
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         #Fill the color with the color set in settings.py
@@ -45,6 +52,7 @@ class AlienInvasion:
         self.ship.blitme()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+        
         
 if __name__ == '__main__':
     # Make a game instance, and run the game.
--- ship.py
+++ ship.py
@@ -12,6 +12,14 @@ class Ship:
         self.rect = self.image.get_rect()
         
         self.rect.midbottom = self.screen_rect.midbottom
+        # Movement flag starts with a ship that is not moving
+        self.moving_right = False
+        
+    def update(self):
+        """Update the ships position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+    
         
     def blitme(self):
         """Draw the ship at its current location."""
