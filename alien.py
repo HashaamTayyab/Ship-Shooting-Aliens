49c8b93f3638fea8743815bf89033e21e3723ebb
hashaam <hashaam@gmail.com>
2023-10-25 20:24:53 +0500

    Added the functionality of a fleet of aliens and moving them right.

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
         """Start the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,3 +19,17 @@ class Alien(Sprite):
         
         # Store the alien exact horizontal position
         self.x = float(self.rect.x)
+        
+    
+    def check_edges(self):
+        """Return True if alien is not at the edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right <= screen_rect) or (self.rect.left <= 0)
+    
+    
+    def update(self):
+        """Moving the alien right or left."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+        
+        
--- settings.py
+++ settings.py
@@ -11,8 +11,15 @@ class Settings:
         # Ship speed settings
         self.ship_speed = 7
         
+        # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 13
+        
+        # ALien settings
+        self.alien_speed = 4.0
+        self.fleet_drop_speed = 10
+        # Fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direcion = 1
